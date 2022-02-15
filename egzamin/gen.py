a4e8164086d43de6c2607e955ead40bedf749a0a
Bruno Kawka <brunokawka@gmail.com>
2022-02-15 14:24:27 +0100

    Add simple exam questions generator

---
+++ egzamin/gen.py
@@ -0,0 +1,31 @@
+import json
+import uuid
+import datetime
+
+FILE_PATH = "baza-pytan.json"
+
+
+def generate_element():
+    id = str(uuid.uuid4())
+    created_at = str(datetime.datetime.now())
+    img = id + ".png"
+    question = ""
+    notes = ""
+    return {
+        "id": id,
+        "created_at": created_at,
+        "img": img,
+        "question": question,
+        "notes": notes,
+    }
+
+
+json_obj = None
+with open(FILE_PATH, "r") as f:
+    json_obj = json.load(f)
+    next_element = generate_element()
+    json_obj.append(next_element)
+
+if json_obj:
+    with open(FILE_PATH, "w", encoding="utf-8") as f:
+        json.dump(json_obj, f, ensure_ascii=False, indent=2, sort_keys=True)
